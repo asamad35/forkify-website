377c2917afcb1fb659eb2ee767e7adaad1614582
asamad35 <samad.abdus3535@gmail.com>
2021-12-11 17:26:08 +0530

    Intial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '311e8b06-db8d-4482-88e0-cc1375ff5cf3';
+export const MODAL_CLOSE_SEC = 2.5;
